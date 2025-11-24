162d10365ff0be8a7401bde1815b4c4d88077ea1
Batiukhno Nikita <nikita@batyukhno-dev.ru>
2025-11-24 22:02:44 +0500

    clear for nvim

--- nvim/lua/plugins/telescope.lua
+++
@@ -1,7 +0,0 @@
-local builtin = require'telescope.builtin'
-
-local alias = require'core.alias'
-alias.nm('<leader>ff', builtin.find_files)
-alias.nm('<leader>fg', builtin.live_grep)
-alias.nm('<leader>fb', builtin.buffers)
-alias.nm('<leader>fh', builtin.help_tags)
