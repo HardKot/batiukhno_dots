5b2e07cc0f608929cfc03b76a52eaa83c971576c
Batiukhno Nikita <nikita@batyukhno-dev.ru>
2026-05-01 16:19:55 +0500

    Restore missing configurate.sh

---
+++ configurate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Создание стандартной структуры директорий пользователя
+echo "Создание структуры папок в домашнем каталоге..."
+
+mkdir -p ~/Documents
+mkdir -p ~/Wallpapers
+mkdir -p ~/Projects
+mkdir -p ~/Music
+mkdir -p ~/Scripts
+mkdir -p ~/Downloads
+mkdir -p ~/Pictures
+mkdir -p ~/Videos
+
+echo "Структура папок создана."
